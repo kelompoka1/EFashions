135dccc9e9dd8e8247d4a263b3f9a4ac61e1f1ae
kelompoka1 <103549374+kelompoka1@users.noreply.github.com>
2022-04-20 08:29:28 +0700

    fix bug & update feature
    changelog 20/04/2022
     1.fixed bug
     2.CRUD employee,supplier added
     3.Redesign frame
     4.additional changes design

--- src/efashion/FrameOwner.java
+++ src/efashion/FrameOwner.java
@@ -33,8 +33,11 @@ public class FrameOwner extends javax.swing.JFrame {
     private void initComponents() {
 
         btn_reset = new javax.swing.JButton();
-        btn_daftarkaryawan = new javax.swing.JButton();
+        btn_editdatasupplier = new javax.swing.JButton();
         btn_dashboard = new javax.swing.JButton();
+        btn_daftarkaryawan1 = new javax.swing.JButton();
+        btn_editstock = new javax.swing.JButton();
+        btn_daftarsupplier1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -43,19 +46,24 @@ public class FrameOwner extends javax.swing.JFrame {
         btn_reset.setBackground(new java.awt.Color(255, 101, 132));
         btn_reset.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         btn_reset.setForeground(new java.awt.Color(255, 255, 255));
-        btn_reset.setText("RESET PASSWORD");
-        getContentPane().add(btn_reset, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 150, 230, 60));
-
-        btn_daftarkaryawan.setBackground(new java.awt.Color(255, 101, 132));
-        btn_daftarkaryawan.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        btn_daftarkaryawan.setForeground(new java.awt.Color(255, 255, 255));
-        btn_daftarkaryawan.setText("DAFTAR KARYAWAN");
-        btn_daftarkaryawan.addActionListener(new java.awt.event.ActionListener() {
+        btn_reset.setText("EDIT DATA KARYAWAN");
+        btn_reset.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_daftarkaryawanActionPerformed(evt);
+                btn_resetActionPerformed(evt);
             }
         });
-        getContentPane().add(btn_daftarkaryawan, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 150, 230, 60));
+        getContentPane().add(btn_reset, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 220, 230, 60));
+
+        btn_editdatasupplier.setBackground(new java.awt.Color(255, 101, 132));
+        btn_editdatasupplier.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btn_editdatasupplier.setForeground(new java.awt.Color(255, 255, 255));
+        btn_editdatasupplier.setText("EDIT DATA SUPPLIER");
+        btn_editdatasupplier.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_editdatasupplierActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btn_editdatasupplier, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 290, 230, 60));
 
         btn_dashboard.setBackground(new java.awt.Color(255, 101, 132));
         btn_dashboard.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
@@ -66,7 +74,40 @@ public class FrameOwner extends javax.swing.JFrame {
                 btn_dashboardActionPerformed(evt);
             }
         });
-        getContentPane().add(btn_dashboard, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 150, 230, 60));
+        getContentPane().add(btn_dashboard, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 220, 230, 60));
+
+        btn_daftarkaryawan1.setBackground(new java.awt.Color(255, 101, 132));
+        btn_daftarkaryawan1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btn_daftarkaryawan1.setForeground(new java.awt.Color(255, 255, 255));
+        btn_daftarkaryawan1.setText("DAFTAR KARYAWAN");
+        btn_daftarkaryawan1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_daftarkaryawan1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btn_daftarkaryawan1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 220, 230, 60));
+
+        btn_editstock.setBackground(new java.awt.Color(255, 101, 132));
+        btn_editstock.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btn_editstock.setForeground(new java.awt.Color(255, 255, 255));
+        btn_editstock.setText("EDIT STOCK BARANG");
+        btn_editstock.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_editstockActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btn_editstock, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 360, 230, 60));
+
+        btn_daftarsupplier1.setBackground(new java.awt.Color(255, 101, 132));
+        btn_daftarsupplier1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btn_daftarsupplier1.setForeground(new java.awt.Color(255, 255, 255));
+        btn_daftarsupplier1.setText("DAFTAR SUPPLIER");
+        btn_daftarsupplier1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_daftarsupplier1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btn_daftarsupplier1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 290, 230, 60));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/efashion/src/formowner.png"))); // NOI18N
         jLabel1.setText("jLabel1");
@@ -79,18 +120,48 @@ public class FrameOwner extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_btn_dashboardActionPerformed
 
-
-    
-
-    private void btn_daftarkaryawanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_daftarkaryawanActionPerformed
+    private void btn_editdatasupplierActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_editdatasupplierActionPerformed
         try {
             this.setVisible(false);
-            new regist_karyawan().setVisible(true);
-            
+            new efashion.featureadmin.editdelete_supplier().setVisible(true);
         } catch (SQLException ex) {
             Logger.getLogger(FrameOwner.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }//GEN-LAST:event_btn_daftarkaryawanActionPerformed
+    }//GEN-LAST:event_btn_editdatasupplierActionPerformed
+
+    private void btn_resetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_resetActionPerformed
+        try {
+            // TODO add your handling code here:
+            new efashion.featureadmin.editdelete_karyawan().setVisible(true);
+            this.setVisible(false);
+        } catch (SQLException ex) {
+            Logger.getLogger(FrameOwner.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }//GEN-LAST:event_btn_resetActionPerformed
+
+    private void btn_daftarkaryawan1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_daftarkaryawan1ActionPerformed
+        // TODO add your handling code here:   
+       this.setVisible(false);
+        try {
+            new efashion.featureadmin.regist_karyawan().setVisible(true);
+        } catch (SQLException ex) {
+            Logger.getLogger(FrameOwner.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_btn_daftarkaryawan1ActionPerformed
+
+    private void btn_editstockActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_editstockActionPerformed
+        this.setVisible(false);
+        //stock frame
+// TODO add your handling code here:
+    }//GEN-LAST:event_btn_editstockActionPerformed
+
+    private void btn_daftarsupplier1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_daftarsupplier1ActionPerformed
+        // TODO add your handling code here
+        this.setVisible(false);
+       new efashion.featureadmin.regist_supplier().setVisible(true);
+       
+    }//GEN-LAST:event_btn_daftarsupplier1ActionPerformed
 
     private void delete(String query,String query2) throws SQLException{
         java.sql.Connection con = (Connection) koneksi.getConDB();
@@ -138,8 +209,11 @@ public class FrameOwner extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btn_daftarkaryawan;
+    private javax.swing.JButton btn_daftarkaryawan1;
+    private javax.swing.JButton btn_daftarsupplier1;
     private javax.swing.JButton btn_dashboard;
+    private javax.swing.JButton btn_editdatasupplier;
+    private javax.swing.JButton btn_editstock;
     private javax.swing.JButton btn_reset;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
